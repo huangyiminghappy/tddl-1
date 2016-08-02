12c171d9547aa3412b4d830dfd172fdcef6e9088
unknown <zhangfangqing@zhangfangqing.zaonline.com>
2016-08-02 17:31:51 +0800

    增加注释

--- tddl-common/src/main/java/com/taobao/tddl/common/config/diamond/DiamondConfigDataHandler.java
+++ tddl-common/src/main/java/com/taobao/tddl/common/config/diamond/DiamondConfigDataHandler.java
@@ -1,19 +1,6 @@
 /*(C) 2007-2012 Alibaba Group Holding Limited.	 *This program is free software; you can redistribute it and/or modify	*it under the terms of the GNU General Public License version 2 as	* published by the Free Software Foundation.	* Authors:	*   junyu <junyu@taobao.com> , shenxun <shenxun@taobao.com>,	*   linxuan <linxuan@taobao.com> ,qihao <qihao@taobao.com> 	*/	package com.taobao.tddl.common.config.diamond;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executor;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.taobao.diamond.manager.DiamondManager;
-import com.taobao.diamond.manager.ManagerListener;
-import com.taobao.diamond.manager.impl.DefaultDiamondManager;
-import com.taobao.tddl.common.config.ConfigDataHandler;
-import com.taobao.tddl.common.config.ConfigDataListener;
-import com.taobao.tddl.common.util.TDDLMBeanServer;
-import com.taobao.tddl.common.util.mbean.TDDLMBean;
+import java.util.List;import java.util.Map;import java.util.concurrent.Executor;import org.apache.commons.logging.Log;import org.apache.commons.logging.LogFactory;import com.taobao.diamond.manager.DiamondManager;import com.taobao.diamond.manager.ManagerListener;import com.taobao.diamond.manager.impl.DefaultDiamondManager;import com.taobao.tddl.common.config.ConfigDataHandler;import com.taobao.tddl.common.config.ConfigDataListener;import com.taobao.tddl.common.util.TDDLMBeanServer;import com.taobao.tddl.common.util.mbean.TDDLMBean;
 
 /**
  * @author shenxun
@@ -24,14 +11,13 @@ import com.taobao.tddl.common.util.mbean.TDDLMBean;
  * @desc 持久配置中心diamond实现
  */
 public class DiamondConfigDataHandler implements ConfigDataHandler {
-	private static final Log logger = LogFactory
-			.getLog(DiamondConfigDataHandler.class);
-	private DiamondManager diamondManager;
+	private static final Log logger = LogFactory.getLog(DiamondConfigDataHandler.class);
+	@SuppressWarnings("deprecation")	private DiamondManager diamondManager;
 	private String dataId;
 	private String mbeanId;
 	private TDDLMBean mbean;
 
-	public void init(final String dataId,
+	@SuppressWarnings("deprecation")	public void init(final String dataId,
 			final List<ConfigDataListener> configDataListenerList,
 			final Map<String, Object> config) {
 		mbean = new TDDLMBean("Diamond Config Info "
@@ -68,7 +54,7 @@ public class DiamondConfigDataHandler implements ConfigDataHandler {
 		TDDLMBeanServer.registerMBeanWithId(mbean, mbeanId);
 	}
 
-	public String getData(long timeout, String strategy) {
+	@SuppressWarnings("deprecation")	public String getData(long timeout, String strategy) {
 		String data = null;
 		if (strategy != null
 				&& strategy
@@ -87,7 +73,7 @@ public class DiamondConfigDataHandler implements ConfigDataHandler {
 		return data;
 	}
 
-	public void addListener(final ConfigDataListener configDataListener,
+	@SuppressWarnings("deprecation")	public void addListener(final ConfigDataListener configDataListener,
 			final Executor executor) {
 		if (configDataListener != null) {
 			diamondManager.setManagerListener(new ManagerListener() {
@@ -107,7 +93,7 @@ public class DiamondConfigDataHandler implements ConfigDataHandler {
 		}
 	}
 
-	public void addListeners(
+	@SuppressWarnings("deprecation")	public void addListeners(
 			final List<ConfigDataListener> configDataListenerList,
 			final Executor executor) {
 		if (configDataListenerList != null) {
@@ -136,7 +122,7 @@ public class DiamondConfigDataHandler implements ConfigDataHandler {
 		}
 	}
 
-	public void closeUnderManager() {
+	@SuppressWarnings("deprecation")	public void closeUnderManager() {
 		diamondManager.close();
 	}
 
